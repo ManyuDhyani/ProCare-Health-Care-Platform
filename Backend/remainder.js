97f003c92b934003ecd00096e623c4f2ba5cac3e
chakravartikothadiya <chakravartikothadiya@gmail.com>
2023-11-30 17:01:23 -0500

    updates in reminder.js

--- Backend/remainder.js
+++ Backend/remainder.js
@@ -1,39 +1,48 @@
-const cron = require('node-cron');
-const nodemailer = require('nodemailer');
+const cron = require("node-cron");
+const nodemailer = require("nodemailer");
+const patientData = require("../Backend/data/patients");
 
 // Replace these values with your Gmail SMTP details
 const smtpConfig = {
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false, // set to true if using SSL
-    auth: {
-      user: "hcare.max.18@gmail.com",
-      pass: "pcqynippnjefmbur",
-    },
-  };
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // set to true if using SSL
+  auth: {
+    user: "hcare.max.18@gmail.com",
+    pass: "pcqynippnjefmbur",
+  },
+};
 
 // Function to send health status email
-const sendHealthStatusEmail = (familyEmail) => {
+const sendHealthStatusEmail = (familyEmail, status) => {
   // Set up email content
   const mailOptions = {
-    from: 'hcare.max.18@gmail.com',
+    from: "hcare.max.18@gmail.com",
     to: familyEmail,
-    subject: 'Patient Health Status Update',
-    text: 'ProCare: Your patient is okay. No cause for concern.',
+    subject: `Patient Health Status Update, Status = ${status}`,
+    text: `ProCare: Your patient is ${status}`,
   };
 
   // Send the email
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
-      console.error('Error sending email:', error.message);
+      console.error("Error sending email:", error.message);
     } else {
-      console.log('Email sent:', info.response);
+      console.log("Email sent:", info.response);
     }
   });
 };
 
 // Schedule the email to be sent every 24 hours
-cron.schedule('0 0 * * *', () => {
+cron.schedule("0 0 * * *", async () => {
   // Basically pass the list of the patient family member
-  sendHealthStatusEmail(['family@example.com']);
+  let allPatientObj = await patientData.getAllPatients();
+
+  allPatientObj.forEach((elem) => {
+    let fimilyMemsArr = elem.familyMembers;
+    let status = elem.status;
+    fimilyMemsArr.forEach((elem2) => {
+      sendHealthStatusEmail(elem2, status);
+    });
+  });
 });
